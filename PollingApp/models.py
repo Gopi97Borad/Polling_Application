810af0d0b04bb20383a0691e4687def45ed669fa
Gopi Borad <gopiborad123@gmail.com>
2021-12-23 22:57:06 +0100

    Update layout of login and registration page

--- PollingApp/models.py
+++ PollingApp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    user_name = models.CharField(max_length=150)
+    user_name = models.CharField(max_length=150, unique=True)
     first_name = models.CharField(max_length=150)
     last_name = models.CharField(max_length=150)
     email = models.CharField(max_length=250)
@@ -11,6 +11,22 @@ class User(models.Model):
 
     class Meta:
         db_table = "User"
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reversed('post', kwargs={'id': self.id})
+
+
+class TaskList(models.Model):
+    user_id = models.IntegerField()
+    task_name = models.CharField(max_length=2000)
+    task_category = models.IntegerField()
+    task_description = models.CharField(max_length=5000)
+
+    class Meta:
+        db_table = "TaskList"
 
     def __str__(self):
         return self.title
--- PollingApp/views.py
+++ PollingApp/views.py
@@ -3,9 +3,11 @@
 # Create your views here.
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 from . import user_login
 from .forms import RegistrationForm
 from .models import *
+from django.contrib import messages
 
 
 def login(request):
@@ -15,14 +17,17 @@ def login(request):
     if request.method == 'POST':
         form = user_login.LoginForm(request.POST)
         if form.is_valid():
-            pass
-            user = form.cleaned_data['username']
-            if user != '':
+            user_name = form.cleaned_data['username']
+            pwd = form.cleaned_data['password']
+            username = User.objects.get(user_name=form.cleaned_data['username'])
+
+            if username.user_name == user_name and username.pwd == pwd:
                 message = 'you have been Loged In'
                 return HttpResponseRedirect('home/')
             else:
-                message = 'Log in Failed'
-    return render(request, 'polls/user_login.html', context={'form': form, 'message': message})
+                messages.info(request,'Your Username or Password is Invalid')
+                return render(request, 'polls/user_login.html', context={'form': form, 'message': message})
+    return render(request, 'polls/user_login.html', context={'form': form})
 
 
 def home(request):
@@ -47,8 +52,10 @@ def vote(request):
 
 def registration(request):
     form = RegistrationForm(request.POST)
+    print(form)
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
+        print(form.is_valid())
         if form.is_valid():
             #form.save()
             return redirect('/')
